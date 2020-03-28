1ad15cd825c1b65442a5adbb90b371ab6fd3726e
Dave Taylor <davetay1021@gmail.com>
2020-03-27 22:50:43 -0400

    add the files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+         <b-navbar-brand href="#">Covid Stats</b-navbar-brand>
+
+         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+         <b-collapse id="nav-collapse" is-nav>
+             <b-navbar-nav>
+         </b-navbar-nav>
+
+         <!-- Right aligned nav items -->
+         <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown text="Countries" right>
+                <b-dropdown-item v-on:click="load('USA')">USA</b-dropdown-item>
+                <b-dropdown-item v-on:click="load('Italy')">Italy</b-dropdown-item>
+                <b-dropdown-item v-on:click="load('China')">China</b-dropdown-item>
+                <b-dropdown-item v-on:click="load('UK')">UK</b-dropdown-item>
+                <b-dropdown-item v-on:click="load('Canada')">Canada</b-dropdown-item>
+                <b-dropdown-item v-on:click="load('Mexico')">Mexico</b-dropdown-item>
+                <b-dropdown-item v-on:click="load('Russia')">Russia</b-dropdown-item>
+                <b-dropdown-item v-on:click="load('Spain')">Spain</b-dropdown-item>
+                <b-dropdown-item v-on:click="load('Brazil')">Brazil</b-dropdown-item>
+            </b-nav-item-dropdown>
+         </b-navbar-nav>
+         </b-collapse>
+         </b-navbar>
+     </div>
+    <router-view ref="childViews" />
   </div>
 </template>
+
+<script>
+
+export default {
+    methods: {
+        load(code) {
+            this.$refs.childViews.loadCountry(code)
+        }
+    }
+}
+
+</script>
 
 <style>
 #app {
---
+++ src/views/Covid.vue
@@ -0,0 +1,71 @@
+<template>
+   <b-container class="mt-5">
+     <b-card-group deck>
+       <b-card v-for="country in countries" :key="country.country" :title="country.country" :img-src="country.countryInfo.flag" img-alt="Image" img-top>
+         <b-card-text>
+            <b-row><b-col align="right">Total Cases:</b-col><b-col align="left">{{ country.cases }}</b-col></b-row>
+            <b-row><b-col align="right">Total Deaths:</b-col><b-col align="left">{{ country.deaths }}</b-col></b-row>
+            <b-row><b-col align="right">Today Cases:</b-col><b-col align="left">{{ country.todayCases }}</b-col></b-row>
+            <b-row><b-col align="right">Today Deaths:</b-col><b-col align="left">{{ country.todayDeaths }}</b-col></b-row>
+            <b-row><b-col align="right">Active Cases:</b-col><b-col align="left">{{ country.active }}</b-col></b-row>
+            <b-row><b-col align="right">Recovered:</b-col><b-col align="left">{{ country.recovered }}</b-col></b-row>
+            <b-row><b-col align="right">Critical:</b-col><b-col align="left">{{ country.critical }}</b-col></b-row>
+         </b-card-text>
+         <template v-slot:footer>
+           <small class="text-muted">Last updated 3 mins ago</small>
+         </template>
+       </b-card>
+     </b-card-group>
+   </b-container> 
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Covid',
+    data: function() {
+        return {
+            countries: []
+        }
+    },
+    computed: {
+        loaded: function() {
+            let list = []
+            for (let country of this.countries) {
+                list.push(country.country)
+            }
+            return list
+        }
+    },
+    created() {
+        for (let c of ['USA', 'UK', 'Italy']) {
+            this.loadCountry(c);
+        }
+    },
+    methods: {
+        loadCountry(code) {
+            for (let country of this.loaded) {
+                if (code == country) {
+                    return false
+                }
+            }
+
+            axios.get('https://corona.lmao.ninja/countries/' + code)
+                .then( resp => {
+                    let country = resp.data
+                    if (this.countries.length < 3) {
+                        this.countries.push(country)
+                    } else {
+                        this.countries.splice(0, 0, country)
+                        this.countries.pop()
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+        }
+    }
+}
+
+</script>
